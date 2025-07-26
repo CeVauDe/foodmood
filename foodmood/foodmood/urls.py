05678c70b9f5e6bad10b0c565891c65dc4ebed55
CeVauDe <cevaude@outlook.de>
2025-07-26 16:17:11 +0200

    Add users registration

--- foodmood/foodmood/urls.py
+++ foodmood/foodmood/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.urls.resolvers import URLPattern, URLResolver
 
 from . import views
 
 urlpatterns: list[URLResolver | URLPattern] = [
     path("", views.index, name="index"),
+    path("users/", include("users.urls")),
     path("admin/", admin.site.urls),
 ]
--- foodmood/users/views.py
+++ foodmood/users/views.py
@@ -1,1 +1,40 @@
-# Create your views here.
+from django import forms
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"Account created for {username}! You can now log in."
+            )
+            login(request, user)  # Automatically log in the user after registration
+            return redirect(
+                "index"
+            )  # Redirect to home page after successful registration
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, "users/register.html", {"form": form})
