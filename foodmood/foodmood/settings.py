25446a2a289526c4f88138684e42e1a43df79683
CeVauDe <cevaude@outlook.de>
2025-08-06 17:07:41 +0200

    Add extending allowed hosts via env vars

--- foodmood/foodmood/settings.py
+++ foodmood/foodmood/settings.py
@@ -38,10 +38,14 @@ SECRET_KEY: str = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG: bool = os.environ.get("DJANGO_DEBUG", "false").lower() == "true"
 
-# As this is currently supposed to only run in local networks, set to all hosts.
-# See https://stackoverflow.com/a/24857636/31156917 for more information
+
 ALLOWED_HOSTS: list[str] = ["localhost", "127.0.0.1", "0.0.0.0"]
 
+allowed_hosts_env: str | None = os.environ.get("ALLOWED_HOSTS", None)
+if allowed_hosts_env is not None:
+    hosts = allowed_hosts_env.split(",")
+    for host in hosts:
+        ALLOWED_HOSTS.append(host.strip())
 
 # Application definition
 
