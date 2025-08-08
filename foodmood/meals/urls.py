d40e922af3561041f5ef1688f03c56d29d715287
CeVauDe <cevaude@outlook.de>
2025-08-08 18:45:23 +0200

    feat: Add create recipe route to urls

--- foodmood/meals/urls.py
+++ foodmood/meals/urls.py
@@ -7,4 +7,5 @@ app_name = "meals"
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create_meal, name="create"),
+    path("recipes/create/", views.create_recipe, name="create_recipe"),
 ]
