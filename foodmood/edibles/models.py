72c1c8522cdd39b5d1e1f81220233483f49aea73
CeVauDe <cevaude@outlook.de>
2025-08-09 18:39:34 +0200

    feat: Add edible model

--- foodmood/edibles/models.py
+++ foodmood/edibles/models.py
@@ -1,1 +1,21 @@
 # Create your models here.
+
+from django.db import models
+
+
+class Edible(models.Model):
+    """
+    An edible item, that can consist of multiple other edibles, but does not have to
+    """
+
+    name: models.CharField = models.CharField(max_length=64)
+    creation_date: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    update_date: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    ingredients: models.ManyToManyField["Edible", "Edible"] = models.ManyToManyField(
+        "Edible", blank=False, related_name="used_in"
+    )
+
+    used_in: models.QuerySet["Edible"]
+
+    def __str__(self) -> str:
+        return f"{self.name}"
--- foodmood/edibles/tests.py
+++ foodmood/edibles/tests.py
@@ -1,1 +1,35 @@
-# Create your tests here.
+from django.test import TestCase
+
+from .models import Edible
+
+
+class IngredientsTestCase(TestCase):
+    def setUp(self) -> None:
+        # Create edibles
+        oil = Edible.objects.create(name="Oil")
+        vinegar = Edible.objects.create(name="Vinegar")
+        salt = Edible.objects.create(name="Salt")
+        pepper = Edible.objects.create(name="Pepper")
+        dressing = Edible.objects.create(name="Dressing")
+        cucumber = Edible.objects.create(name="Cucumber")
+        lettuce = Edible.objects.create(name="Lettuce")
+        salad = Edible.objects.create(name="Mixed Salad")
+
+        # Set ingredients
+        dressing.ingredients.set([oil, vinegar, salt, pepper])
+        salad.ingredients.set([dressing, cucumber, lettuce])
+
+    def test_ingredients_count(self) -> None:
+        dressing = Edible.objects.get(name="Dressing")
+        self.assertEqual(dressing.ingredients.count(), 4)
+
+        salad = Edible.objects.get(name="Mixed Salad")
+        self.assertEqual(salad.ingredients.count(), 3)
+
+    def test_used_in_count(self) -> None:
+        dressing = Edible.objects.get(name="Dressing")
+        self.assertEqual(dressing.used_in.count(), 1)
+
+    # def test_ingredients_count_of_ingredient(self) -> None:
+    #     salad = Edible.objects.get(name="Mixed Salad")
+    #     self.assertEqual(salad.ingredients.get(name="Dressing").ingredients.count(), 4)
