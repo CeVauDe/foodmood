a0ff4648be5147d14fe7fd659e3803643b1f39e0
CeVauDe <cevaude@outlook.de>
2025-08-11 17:16:37 +0200

    fix: Add http method for index url

--- foodmood/edibles/views.py
+++ foodmood/edibles/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpRequest, HttpResponse
+from django.views.decorators.http import require_safe
 
 
 # Create your views here.
+@require_safe
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index.")
